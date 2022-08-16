e2deed1a2a4f1043279afb35965c85896a77f8b5
marvin.b7 <marvin.b7@P-0G-0B113-0446>
2022-08-16 14:13:58 +0200

    Grundgerüst

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connetionString = "host=localhost;user=root;database=manipulation";
+
+            using (MySqlConnection con = new MySqlConnection(connetionString))
+            {
+                try
+                {
+                    con.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
