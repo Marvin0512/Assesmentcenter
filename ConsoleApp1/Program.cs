4c84c4ed13da54a0ab2c7de1cdf5e42d6e1bc8ca
marvin.b7 <marvin.b7@P-0G-0B113-0446>
2022-09-06 12:40:51 +0200

    Manipulation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,19 +14,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string connetionString = "host=localhost;user=root;database=manipulation";
+            string connetionString = "host=localhost;port=3301;user=root;database=datamanipulation";
 
             using (MySqlConnection con = new MySqlConnection(connetionString))
             {
                 try
                 {
                     con.Open();
+                    //------Hinzufügen-----
+                    MySqlCommand command = new MySqlCommand("INSERT INTO `nahrungsmittel` (`id`, `name`, `mindesthaltbarkeitsdatum`) VALUES ('8', 'Pohl', '2069-01-03');", con);
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("Glück gehabt du Lappen");
+
+                    //------Entfernen------
+                    MySqlCommand command2 = new MySqlCommand("DELETE FROM `nahrungsmittel` WHERE `nahrungsmittel`.`id` = 8;", con);
+                    command2.ExecuteNonQuery();
+
+                    Console.WriteLine("Du hast einen Lauf oder so             du Lappen");
+
+                    //-----ändern---------
+                    MySqlCommand command3 = new MySqlCommand("UPDATE `nahrungsmittel` SET `mindesthaltbarkeitsdatum` = '2050-05-02 00:00:00' WHERE `nahrungsmittel`.`id` = 7;", con);
+                    command3.ExecuteNonQuery();
+
+                    Console.WriteLine("Bu bist trozdem Dumm");
                 }
                 catch (Exception ex)
                 {
                     Console.Write(ex);
                 }
-                Console.ReadKey();
+                Console.ReadKey();  
             }
         }
     }
